97e7b73bfbeb8fa7d50c661d72e648f0ff38b7e4
Silvia <silviaperrino93@gmail.com>
2015-11-17 16:48:40 +0100

    fixed close tab button

--- static/js/init.js
+++ static/js/init.js
@@ -86,26 +86,29 @@ $( document ).ready(function() {
                     + addZero(currentdate.getMinutes());
 
 
-    $(function() { //replace(/(/)/g, '');
-      addTab = function(text, url){
-            $("ul.nav.nav-tabs").append("<li><a data-toggle='tab' href='#"+url+"'>Doc<button class='close closeTab' type='button' onclick='closeTab(this)'>x</button></a></li>");
+    $(function() {
+      addTab = function(text, url, title){
+            var url = url.replace(/([/|_.|_:|_-])/g, '');
+            $("ul.nav.nav-tabs").append("<li><a class='box' data-toggle='tab' href='#"+url+"' role='tabpanel'><label>"+title+"</label><span class='glyphicon glyphicon-remove' title='Chiudi' onclick='closeTab(this)'></span></a></li>");
             $("div.tab-content").append("<div class='tab-pane fade' id='"+url+"'><div id='"+url+"t'></div></div>");
             $("#"+url+"t").html(text);
-       }
+
+//            $('div#tabs').tabs();
+//            $('div#tabs').tabs('select', url+'t');
+        }
     });
-
-
 
 });
 
 function mostraDocumento(element){
+    var title = $(element).text()
     var urlDoc = $(element).attr('value')
     $.ajax({
         url: '/scrapingSingoloDocumento',
         type: 'GET',
         data: {url: urlDoc},
         success: function(result) {
-            singoloDocumento(result, urlDoc);
+            singoloDocumento(result, urlDoc, title);
         },
         error: function(error) {
             alert("Error: " + error);
@@ -113,14 +116,15 @@ function mostraDocumento(element){
     });
 }
 
-function singoloDocumento(str, url){
+function singoloDocumento(str, url, title){
     //$("#singoloDocumento").html(str);
-    addTab(str, url);
+    addTab(str, url, title);
 }
 
 function closeTab(element){
     var tabContentId = $(element).parent().attr("href");
     $(element).parent().parent().remove(); //remove li of tab
-    $('ul.nav.nav-tabs a:last').tab('show'); // Select first tab
     $(tabContentId).remove(); //remove respective tab content
+    $('ul.nav.nav-tabs a:last').tab('show'); // Select first tab
+
 }
